d0b699fc72f8463e8d49dff45cff85579665d500
zorowatt <zorowatt@gmail.com>
2014-10-19 09:34:59 +0300

    45

--- public/app/controllers/homeCtrl.js
+++ public/app/controllers/homeCtrl.js
@@ -2,7 +2,10 @@ app.controller('HomeCtrl',['$scope','$resource','$http','$q','identity', functio
     $scope.identity = identity; //this is only to show Add Product button if logged user exists
 
 
-
+    var $viewportMeta = $('meta[name="viewport"]');
+    $('input, select, textarea').bind('focus blur', function(event) {
+        $viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' +        (event.type == 'blur' ? 10 : 1));
+    });
 // TODO gets any element on click
 
 //    function getEl(){
