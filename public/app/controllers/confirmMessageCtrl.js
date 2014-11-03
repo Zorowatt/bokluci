61cbe8c05811253b60d455599766ea49f11123a8
zorowatt <zorowatt@gmail.com>
2014-11-03 10:53:54 +0200

    120

--- public/app/controllers/confirmMessageCtrl.js
+++
@@ -1,9 +0,0 @@
-app.controller('ConfirmMessageCtrl',['$scope','$modalInstance','message', function ($scope,$modalInstance,message) {
-    $scope.message = message;
-    $scope.close = function () {
-        $modalInstance.close('close');
-    };
-    $scope.no = function () {
-        $modalInstance.dismiss();
-    };
-}]);
